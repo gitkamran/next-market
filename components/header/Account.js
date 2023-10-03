beb2d1d5804138e75c90307a73fb1d3e88d34ec5
gitkamran <mkamran.ir@gmail.com>
2023-10-04 00:47:17 +0330

    add login component

--- components/header/Account.js
+++ components/header/Account.js
@@ -1,13 +1,15 @@
 "use client"
 import { useAppContext } from '@/context/appContext/AppContext';
-import React from 'react'
+import React, { useState } from 'react'
 import { HiOutlineShoppingBag, HiOutlineUserCircle } from 'react-icons/hi2'
+import LoginComp from '../login-comp/LoginComp';
 
 const Account = () => {
     const { cartNumber, setCartNumber } = useAppContext();
+    const [showLogin, setShowLogin] = useState(false)
     return (
-        <div className='flex items-center gap-2'>
-            <button className='outline outline-1 outline-rose-500/90 p-2 rounded-md flex items-center gap-1 hover:shadow-md hover:shadow-rose-300 duration-200'>
+        <div className='relative flex items-center gap-2'>
+            <button onClick={() => setShowLogin(!showLogin)} className='outline outline-1 outline-rose-500/90 p-2 rounded-md flex items-center gap-1 hover:shadow-md hover:shadow-rose-300 duration-200'>
                 <HiOutlineUserCircle className='text-rose-500/90 text-2xl' />
                 <span className='text-rose-500/90'>ورود به حساب کاربری</span>
             </button>
@@ -15,6 +17,9 @@ const Account = () => {
                 <span className='bg-white rounded-md w-6 h-6 flex items-center justify-center text-neutral-600'>{cartNumber}</span>
                 <HiOutlineShoppingBag className='text-white text-2xl' />
             </button>
+            {showLogin &&
+                <LoginComp setShowLogin={setShowLogin} />
+            }
         </div>
     )
 }
